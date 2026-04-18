04c38be9102334f4840dc6bb82a0c204a3007bd4
apopr <arlisepopruzuk@gmail.com>
2026-04-18 12:08:55 -0400

    text shadow change, greeting change, footer header change

--- interact.js
+++ interact.js
@@ -3,5 +3,5 @@ function enterName()
     let name = document.getElementById("name").value;
     let greeting = document.getElementById("greeting");
 
-    greeting.innerHTML = "Thank you for your message  " + name + "!";
+    greeting.innerHTML = "Thank you for your message, we will be in touch!";
 }
